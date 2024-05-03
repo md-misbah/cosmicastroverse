5a01d6c31f24b926438983fe8cb812eb818fa9ed
DeveloperMisbah <mdmisbah.dev@gmail.com>
2024-05-03 23:29:25 +0600

    first commit

---
+++ components/CopyRight.js
@@ -0,0 +1,8 @@
+
+export default function CopyRight() {
+  return (
+    <div className="glassEfl border-r-0 p-4 w-full mt-20">
+      <h1 className="text-white w-fit mx-auto">&copy; Cosmic Astro-Verse || 2024-2025 || All rights reserved.</h1>
+    </div>
+  )
+}
