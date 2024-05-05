a0f24583cc3ff067e74d866f37bca9d576c9ccc9
Md Misbah <80062894+md-misbah@users.noreply.github.com>
2024-05-05 21:47:36 +0600

    Update CopyRight.js

--- components/CopyRight.js
+++ components/CopyRight.js
@@ -2,7 +2,7 @@
 export default function CopyRight() {
   return (
     <div className="glassEfl border-r-0 p-4 w-full mt-20">
-      <h1 className="multiColorTxt w-fit font-bold mx-auto text-center">&copy; Cosmic Astro-Verse || 2024-2025 || All rights reserved.</h1>
+      <h1 className="multiColorTxt w-fit font-bold mx-auto text-center">&copy; Cosmic-Verse || 2024-2025 || All rights reserved.</h1>
     </div>
   )
 }
